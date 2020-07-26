1c0c173c25e632bace3d62c93eb073e4e8f018e7
Hasko <hoodhasko@yandex.ru>
2020-07-26 13:31:34 +0500

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className='header'>
+      <img
+        src='https://img.icons8.com/wired/64/000000/social-network.png'
+        alt='icon'
+      />
+    </header>
+  );
+};
+
+export default Header;
