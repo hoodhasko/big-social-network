64537d7779883dadc884bb7689fad1872ae31e3c
Hasko <hoodhasko@yandex.ru>
2020-07-28 22:01:55 +0500

    Complete lesson 19. Add new component Dialogs.

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import MyPosts from './MyPosts/MyPosts';
-import s from './Profile.module.css';
 
 const Profile = () => {
   return (
-    <div className={s.content}>
+    <div>
       Profile
       <MyPosts />
     </div>
