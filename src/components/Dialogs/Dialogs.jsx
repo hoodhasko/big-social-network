5f4b0a172516eb8eea8101ea15968cec37825ca5
Hasko <hoodhasko@yandex.ru>
2020-07-30 22:16:50 +0500

    Complete lessons 21 and 22. Add structure dialogs and routing.

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
+import s from './Dialogs.module.css';
+import {NavLink} from "react-router-dom";
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    <NavLink to='/dialogs/1'>Alex</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to='/dialogs/2'>Hasko</NavLink>
+                </div><div className={s.dialog}>
+                    <NavLink to='/dialogs/3'>Smd</NavLink>
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hey</div>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>Yo</div>
+            </div>
         </div>
     );
-}
+};
 
-export  default Dialogs;
+export default Dialogs;
