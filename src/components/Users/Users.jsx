bec8f2cbfdff571569a3cdf4a29f22e6257feefa
Alex Hasko <ghost08-94@yandex.ru>
2021-03-09 14:39:33 +0500

    Преобразовали функциональную компоненты в классовую.

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -5,32 +5,39 @@ import './Users.module.css';
 
 import avatar from '../../assets/images/avatar.png';
 
-let Users = (props) => {
+class Users extends React.Component {
+	constructor(props) {
+		super(props);
 
-	if (props.users.length === 0 ) {
 		axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
 			console.log(response)
-			props.getUsers(response.data.items);
+			this.props.getUsers(response.data.items);
 		});
 	}
 
-	return <div>
-		{
-			props.users.map(u => <div key={u.id}>
-				<img src={u.photos.small != null ? u.photos.small : avatar} alt="avatar"/>
-				<div>
-					{u.followed ? 
-					<button onClick={() => {props.unfollow(u.id)}}>Unfollow</button> : 
-					<button onClick={() => {props.follow(u.id)}}>Follow</button>}
-				</div>
-				<p>{u.name}</p>
-				<p>{u.status}</p>
-				{/*<p>{u.location.country}</pdsds>*/}
-				{/*<p>{u.location.city}</p>*/}
-			</div>)
-		}
+    render() {
+        return (
+            <div>
+                {
+                    this.props.users.map(u => <div key={u.id}>
+                        <img src={u.photos.small != null ? u.photos.small : avatar} alt="avatar"/>
+                        <div>
+                            {u.followed ?
+                                <button onClick={() => {
+                                    this.props.unfollow(u.id)
+                                }}>Unfollow</button> :
+                                <button onClick={() => {
+                                    this.props.follow(u.id)
+                                }}>Follow</button>}
+                        </div>
+                        <p>{u.name}</p>
+                        <p>{u.status}</p>
+                    </div>)
+                }
+            </div>
+        );
+    }
 
-	</div>
 }
 
 export default Users;
